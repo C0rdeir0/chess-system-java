586632abfa039a83d3ed8465bc7ea2f0f08e2d51
Ewerton Cordeiro <ewertonecordeiro@gmail.com>
2023-03-26 17:56:23 -0300

    first class: Position

---
+++ src/applicatin/Program.java
@@ -0,0 +1,11 @@
+package applicatin;
+
+import boardgame.Position;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3, 5);
+		System.err.println(pos);
+	}
+}
